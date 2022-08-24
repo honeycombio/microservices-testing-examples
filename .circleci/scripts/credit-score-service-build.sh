14362e29a3c7b7e02961b0b275249efaf1c9928f
Abby Bangser <bangser.a@gmail.com>
2022-08-24 21:19:54 +0100

    setting env vars

--- .circleci/scripts/credit-score-service-build.sh
+++ .circleci/scripts/credit-score-service-build.sh
@@ -9,4 +9,4 @@ mvn verify -pl credit-score-service -Pprovider-pacts -Dpact.verifier.publishResu
 # can-i-deploy
 ## no need to run since it doesn't create any pacts
 # tag pacts as production
-docker run --rm --network="internal" ${PACT_CLI_IMG} broker create-version-tag --pacticipant credit-score-service --version `git rev-parse --short HEAD` --tag prod --broker-base-url ${PACT_BROKER_URL} --broker-username=rw_user --broker-password=rw_pass
+docker run --rm --net host ${PACT_CLI_IMG} broker create-version-tag --pacticipant credit-score-service --version `git rev-parse --short HEAD` --tag prod --broker-base-url ${PACT_BROKER_URL} --broker-username=rw_user --broker-password=rw_pass
