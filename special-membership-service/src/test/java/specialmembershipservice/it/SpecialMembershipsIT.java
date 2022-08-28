28f15e2b47c1e59cc7dde2be4aa849e6c3c7610c
Abby Bangser <bangser.a@gmail.com>
2022-08-28 15:35:59 +0100

    set schedules back up

--- special-membership-service/src/test/java/specialmembershipservice/it/SpecialMembershipsIT.java
+++ special-membership-service/src/test/java/specialmembershipservice/it/SpecialMembershipsIT.java
@@ -30,7 +30,7 @@ public class SpecialMembershipsIT extends IntegrationTestBase {
     Map<String, Object> specialMembershipDto = specialMembershipDto(email);
     Response response = resourcesClient.postSpecialMembership(specialMembershipDto);
     response.close();
-    assertThat(response.getStatus(), equalTo(100));
+    assertThat(response.getStatus(), equalTo(generateRandomStatusCode()));
     verifyPublishedMemberSignedUpEvent(email);
   }
 
