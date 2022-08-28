3de84239f035cfaa6a8ab64860ca0ceafe1118d3
Abby Bangser <bangser.a@gmail.com>
2022-08-28 15:31:41 +0100

    intentionally fail test

--- special-membership-service/src/test/java/specialmembershipservice/it/SpecialMembershipsIT.java
+++ special-membership-service/src/test/java/specialmembershipservice/it/SpecialMembershipsIT.java
@@ -30,7 +30,7 @@ public class SpecialMembershipsIT extends IntegrationTestBase {
     Map<String, Object> specialMembershipDto = specialMembershipDto(email);
     Response response = resourcesClient.postSpecialMembership(specialMembershipDto);
     response.close();
-    assertThat(response.getStatus(), equalTo(generateRandomStatusCode()));
+    assertThat(response.getStatus(), equalTo(100));
     verifyPublishedMemberSignedUpEvent(email);
   }
 
