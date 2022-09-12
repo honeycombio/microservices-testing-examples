5fee8b0f8c8ca87cf31b69be47cc53cbeb03c753
brookesargent <brookesargent01@gmail.com>
2022-09-12 16:02:05 -0400

    tf for queries and board

--- terraform-examples/main.tf
+++ terraform-examples/main.tf
@@ -7,11 +7,101 @@ terraform {
   }
 }
 
-# Configure provider
-provider "honeycombio" {}
-
-# Create a marker
-resource "honeycombio_marker" "hello" {
-  message = "Hello world!"
+locals {
   dataset = "buildevents"
+  time_range = 604800
+}
+
+data "honeycombio_query_specification" "duration_heatmap" {
+  calculation {
+    op     = "HEATMAP"
+    column = "duration_ms"
+  }
+
+  filter {
+    column = "workflow.status"
+    op     = "="
+    value  = "success"
+  }
+
+  filter {
+    column = "trace.parent_id"
+    op     = "does-not-exist"
+  }
+
+  time_range = local.time_range
+}
+
+resource "honeycombio_query" "duration_heatmap" {
+  dataset    = local.dataset
+  query_json = data.honeycombio_query_specification.duration_heatmap.json
+}
+
+data "honeycombio_query_specification" "success_failure_breakdown" {
+  calculation {
+    op     = "COUNT"
+  }
+
+  filter {
+    column = "trace.parent_id"
+    op     = "does-not-exist"
+  }
+
+  breakdowns = ["workflow.status"]
+  time_range = local.time_range
+}
+
+resource "honeycombio_query" "success_failure_breakdown" {
+  dataset    = local.dataset
+  query_json = data.honeycombio_query_specification.success_failure_breakdown.json
+}
+
+data "honeycombio_query_specification" "build_times_over_15_min" {
+  calculation {
+    op     = "COUNT"
+  }
+
+  filter {
+    column = "workflow.status"
+    op     = "="
+    value  = "success"
+  }
+
+  filter {
+    column = "trace.parent_id"
+    op     = "does-not-exist"
+  }
+
+  filter {
+    column = "duration_ms"
+    op = ">"
+    value = 900000
+  }
+
+  time_range = local.time_range
+}
+
+resource "honeycombio_query" "build_times_over_15_min" {
+  dataset    = local.dataset
+  query_json = data.honeycombio_query_specification.build_times_over_15_min.json
+}
+
+resource "honeycombio_board" "buildevents_board" {
+  name        = "Buildevents Board"
+  style       = "visual"
+
+  query {
+    caption = "Duration Heatmap"
+    query_id = honeycombio_query.duration_heatmap.id
+  }
+
+  query {
+    caption = "Breakdown by Success/Failure"
+    query_id = honeycombio_query.success_failure_breakdown.id
+  }
+
+  query {
+    caption = "Build Times > 15 min"
+    query_id = honeycombio_query.build_times_over_15_min.id
+  }
 }
