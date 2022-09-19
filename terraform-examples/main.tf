9dbf35481381667ad498a1245f64e26987c29ebb
MJ <mridulajayaraman@honeycomb.io>
2022-09-19 14:45:58 -0700

    update tf cloud workspace

--- terraform-examples/main.tf
+++ terraform-examples/main.tf
@@ -1,9 +1,9 @@
 terraform {
   cloud {
-    organization = "mj-test"
+    organization = "honeycomb"
 
     workspaces {
-      name = "guide-testing"
+      name = "buildevents-quick-start-guides"
     }
   }
 }
